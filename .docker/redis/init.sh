b68b2793fc6324b3d23f2e1a9591a9972ff61549
carry0987 <carry0987@users.noreply.github.com>
2023-11-08 17:40:58 +0800

    Fix Redis shutdown process

--- .docker/redis/init.sh
+++ .docker/redis/init.sh
@@ -12,5 +12,21 @@
 # the sum of the size of physical memory and the size of exchange workspace to the process.
 sysctl vm.overcommit_memory=1
 
-# Start redis server
-redis-server /usr/local/etc/redis/redis.conf --requirepass "${REDIS_PASSWORD}" --loglevel warning --bind 0.0.0.0
+# Define a function that shuts down Redis gracefully
+stop_redis() {
+    echo "Shutting down Redis..."
+    redis-cli -a "$REDIS_PASSWORD" shutdown
+    exit 0
+}
+
+# Trap TERM and INT signals and call stop_redis function
+trap "stop_redis" TERM INT
+
+# Start Redis server in the background
+redis-server /usr/local/etc/redis/redis.conf \
+    --requirepass "$REDIS_PASSWORD" \
+    --loglevel warning \
+    --bind 0.0.0.0 &
+
+# Keep the script running by waiting for the background Redis server to finish
+wait
